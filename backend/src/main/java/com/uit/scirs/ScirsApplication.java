c8388f9cf705d03176e07c4916d3f8c08739f29b
Alphathanlwin <aungthan.vip123@gmail.com>
2026-08-13 10:27:25 +0630

    chore: implement springboot config scaffolding

---
+++ backend/src/main/java/com/uit/scirs/ScirsApplication.java
@@ -0,0 +1,12 @@
+package com.uit.scirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScirsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScirsApplication.class, args);
+    }
+}
